28035aaf19170b51693bc8f791c79fe91cffaf17
nora <usernamesaresoharddtomake@gmail.com>
2023-06-19 16:37:32 +0800

    create sidebar & main

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#2E2C2B',
+        secondary: '#abaab1',
+        tertiary: '#677FE5',
+        colorScheme1: '#E6FAFA',
+        colorScheme2: '#EBEEFD',
+      },
+      fontFamily: {
+        inter: ['Inter']
+      }
+    },
   },
   plugins: [],
 }
