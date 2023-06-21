e722dd303e12f4276ce1ed41c20b93de769b3d3a
nora <usernamesaresoharddtomake@gmail.com>
2023-06-21 15:10:22 +0800

    fix(cal): class style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: [
+    './index.html',
+    './script.js'
+  ],
   theme: {
     extend: {
       colors: {
